6cd165b53fdb964c8bc69627bfd9760241e8ef98
Arunava-27 <drarunkundu22@gmail.com>
2023-11-19 17:33:46 +0530

    change frontend framework'

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
 import connectDB from "./config/db.js";
 import userRoutes from "./routes/userRoutes.js";
 import { notFound, errorHandler } from "./middleware/errorMiddleWare.js";
@@ -10,6 +11,7 @@ const port = process.env.PORT || 5000;
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 
 app.get("/", (req, res) => {
   res.send("Redux POC App");
@@ -20,8 +22,13 @@ app.use("/api/users", userRoutes);
 app.use(notFound);
 app.use(errorHandler);
 
-connectDB(
-  app.listen(port, () => {
-    console.log(`Server running on port ${port}`);
+connectDB()
+  .then(() => {
+    app.listen(port, () => {
+      console.log(`Server running on port ${port}`);
+    });
   })
-);
+  .catch((err) => {
+    console.log(err);
+    process.exit(1);
+  });
--- backend/controller/userController.js
+++ backend/controller/userController.js
@@ -3,55 +3,101 @@ import User from "../models/userModel.js";
 import generateToken from "../utils/generateToken.js";
 
 export const authUser = asyncHandler(async (req, res) => {
-  res.status(200).json({
-    message: "Auth user",
-  });
+  const { email, password } = req.body;
+
+  const user = await User.findOne({ email });
+
+  if (user && (await user.matchPassword(password))) {
+    generateToken(res, user._id);
+    res.status(200).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+    });
+  } else {
+    res.status(401);
+    throw new Error("Invalid email or password");
+  }
 });
 
 export const logoutUser = asyncHandler(async (req, res) => {
+  res.cookie("jwt", "", {
+    httpOnly: true,
+    expires: new Date(0),
+  });
   res.status(200).json({
-    message: "Logout user",
+    message: "User logged out",
   });
 });
 
 export const registerUser = asyncHandler(async (req, res) => {
   const { name, email, password } = req.body;
-  
+
   const userExists = await User.findOne({ email });
 
-    if (userExists) {
-        res.status(400).json({
-        message: "User already exists",
-        });
-    }
-
-    const user = await User.create({
-        name,
-        email,
-        password,
+  if (userExists) {
+    res.status(400).json({
+      message: "User already exists",
     });
+  }
 
-    if (user) {
-        generateToken(res, user._id)
-        res.status(201).json({
-        _id: user._id,
-        name: user.name,
-        email: user.email,
-        });
-    } else {
-        res.status(400)
-        throw new Error("Invalid user data");
-    }
+  const user = await User.create({
+    name,
+    email,
+    password,
+  });
+
+  if (user) {
+    generateToken(res, user._id);
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+    });
+  } else {
+    res.status(400);
+    throw new Error("Invalid user data");
+  }
 });
 
 export const getUserProfile = asyncHandler(async (req, res) => {
-  res.status(200).json({
-    message: "Get user profile",
-  });
+
+  const user = await User.findById(req.user._id);
+
+  if (user) {
+    res.status(200).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+    });
+  } else {
+    res.status(404);
+    throw new Error("User not found");
+  }
 });
 
 export const updateUserProfile = asyncHandler(async (req, res) => {
-  res.status(200).json({
-    message: "Update user profile",
-  });
+  
+  const user = await User.findById(req.user._id);
+
+  if (user) {
+    user.name = req.body.name || user.name;
+    user.email = req.body.email || user.email;
+
+    if (req.body.password) {
+      user.password = req.body.password;
+    }
+
+    const updatedUser = await user.save();
+
+    generateToken(res, updatedUser._id);
+    res.status(200).json({
+      _id: updatedUser._id,
+      name: updatedUser.name,
+      email: updatedUser.email,
+    });
+  } else {
+    res.status(404);
+    throw new Error("User not found");
+  }
 });
